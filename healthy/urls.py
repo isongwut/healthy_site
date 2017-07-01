dae682c282670f255eacf91d9edee0b2f7be49ee
user <a5710131a@gmail.com>
2017-07-01 17:15:39 +0700

    editadd comment view.py and url.py

--- healthy/urls.py
+++ healthy/urls.py
@@ -5,23 +5,27 @@ from . import views
 app_name = 'healthy'
 urlpatterns = [
 
-    url(r'^$', views.home_page, name='home_page'),
+    url(r'^$', views.home_page, name='home_page'), #  home_page
 
-    url(r'^about$', views.about, name='about'),
+    url(r'^about$', views.about, name='about'),    #  about page
     
-    url(r'^detail_food$', views.detail_food, name='detail_food'),
+    url(r'^detail_food$', views.detail_food, name='detail_food'), #  detail_food page
 
-    url(r'^select_menu_page$', views.select_menu_page, name='select_menu_page'),
+    # select menu
+    url(r'^select_menu_page$', views.select_menu_page, name='select_menu_page'), # none menu in foodlist
     
-    url(r'^select_food$', views.select_food, name='select_food'),
-
-    url(r'^bmr$', views.bmr, name='bmr'),
-
-    url(r'^cal_bmr$', views.cal_bmr, name='cal_bmr'),
+    url(r'^select_food$', views.select_food, name='select_food'),                # have menu in foodlist
     
-    url(r'^cal_bmr/(?P<excess_calories>-?[0-9]+)/$', views.exercise, name='exercise'),
+    # BMR
 
-    url(r'^cal_bmr/(?P<excess_calories>-?[0-9]+)/burn/$', views.burn_calories, name='burn_calories'),
+    url(r'^bmr$', views.bmr, name='bmr'),             # bmr page receive value for calculate bmr
 
+    url(r'^cal_bmr$', views.cal_bmr, name='cal_bmr'), # calculate bmr
+    
+    # exercise
+    
+    url(r'^cal_bmr/(?P<excess_calories>-?[0-9]+)/$', views.exercise, name='exercise'), # exercise page 
+
+    url(r'^cal_bmr/(?P<excess_calories>-?[0-9]+)/burn/$', views.burn_calories, name='burn_calories'), # cal excess_calories for time burn
 
 ]
--- healthy/views.py
+++ healthy/views.py
@@ -5,95 +5,96 @@ from healthy.models import Food,FoodList,Exercise
 from django.shortcuts import get_object_or_404
 
 
-def home_page(request):
-    FoodList.objects.all().delete()
-    return render(request, 'home.html')
+def home_page(request):  		# home page
+    FoodList.objects.all().delete()     # clear data in list menu
+    return render(request, 'home.html') # render template home
 
-def about(request):
-    return render(request, 'about.html')
+def about(request):                      # about page 
+    return render(request, 'about.html') # render template about
 
-def detail_food(request):
-    contex = {'foods' : Food.objects.all()}
-    return render(request, 'detail_food.html',contex)
+def detail_food(request):                              # detail food page 
+    contex = {'foods' : Food.objects.all()}            # argument(food) of send to template
+    return render(request, 'detail_food.html',contex)  # render template detail_food
 
-def select_menu_page(request):
-    contex = {'foods' : Food.objects.all(), 'foodList_select' : FoodList.objects.all(), 'calories_total' : sum_calories(FoodList.objects.all())}
-    return render(request, 'start_select_food.html', contex)
+def select_menu_page(request):                              # select_menu_page                     
+    contex = {'foods' : Food.objects.all()}                 # argument(food) of send to template
+    return render(request, 'select_food_page.html', contex) # render template select_food_page
 
-def select_food(request):
-    calories_total = 0
+def select_food(request):     # function select menu
+    calories_total = 0        # set start value of calories_total
     
     # clear
-    if request.method == 'GET':
-        FoodList.objects.all().delete()
-        contex = {'foods' : Food.objects.all(), 'foodList_select' : FoodList.objects.all(), 'calories_total' : sum_calories(FoodList.objects.all())}
-        #return render(request, 'select_food.html', contex)
-        return redirect('/select_menu_page')
+    if request.method == 'GET':                 # when request method GET
+        FoodList.objects.all().delete()         # clear data in list menu
+        contex = {'foods' : Food.objects.all()} # argument(food) of send to template
+        return redirect('/select_menu_page')    # redirect back to url /select_menu_page
 
     # select 
-    select = Food.objects.get(name = request.POST.get('food'))
-    food_texts = select.name
-    number_food = request.POST.get('number_food')
-    calories = select.calories * int(number_food)
-    FoodList.objects.create(name = food_texts,number_per_menu = number_food , calories_per_menu = calories)
-  
+    select = Food.objects.get(name = request.POST.get('food')) # Get object menu from model Food
+    food_texts = select.name                                   # name menu at select
+    number_food = request.POST.get('number_food')              # number of menu at select
+    calories = select.calories * int(number_food)              # calculate calories and number
+
+    FoodList.objects.create(name = food_texts,number_per_menu = number_food , calories_per_menu = calories) # save nemu select to model FoodList(database)
     
     # calories total
-    calories_total = sum_calories(FoodList.objects.all())
+    calories_total = sum_calories(FoodList.objects.all())      # calculate calories total from FoodList
      
-    contex = {'foods' : Food.objects.all(), 'foodList_select' : FoodList.objects.all(), 'calories_total' : calories_total}
-    return render(request, 'select_food.html', contex)
+    contex = {'foods' : Food.objects.all(), 'foodList_select' : FoodList.objects.all(), 'calories_total' : calories_total} # argument of send to template
+    return render(request, 'select_food.html', contex)         # render template select_food for show table menu select
 
-def bmr(request):
-    return render(request, 'cal_bmr.html')
+def sum_calories(foodList):      # function sum_calories for FoodList
+    calories_total = 0       
+    for food in foodList:        # for all data in FoodList
+        calories_total += food.calories_per_menu   # sum calories
+    return calories_total        # return value
+
+def bmr(request):                              # bmr page
+    return render(request, 'cal_bmr.html')     # render template cal_bmr for receive data calculate BMR
  
-
-def exercise(request,excess_calories):
-    contex = {'exercises' : Exercise.objects.all(),'excess_calories' : excess_calories}
-    return render(request, 'exercise.html',contex)
-
-def cal_bmr(request):
-    bmr = 0
-    calories_select_total = 0
-    excess_calories = 0
-    foods = Food.objects.all()
-    exercises = Exercise.objects.all()
+def cal_bmr(request):                   # function sum_calories for FoodList
+    bmr = 0                             # set zero BMR value each one
+    calories_select_total = 0           # set zero calories_select_total for select menu
+    excess_calories = 0                 # set zero excess_calories for go to calculate burn_calories
+    foods = Food.objects.all()          # All menu 
+    exercises = Exercise.objects.all()  # All exercises
 
     # calories total
-    calories_select_total = sum_calories(FoodList.objects.all())
+    calories_select_total = sum_calories(FoodList.objects.all()) # calculate calories total from FoodList
 
     # cal bmr
-    sex = request.POST.get('sex')
-    height = int(request.POST.get('height'))
-    weight = int(request.POST.get('weight'))
-    age = int(request.POST.get('age'))
-    if sex == 'Male' :
-        bmr = int(66+((13.7*weight)+(5*height)-(6.8*age)))
-    else:
-        bmr = int(665+((9.6*weight)+(1.8*height)-(4.7*age)))
+    sex = request.POST.get('sex')             # receive sex value from method post
+    height = int(request.POST.get('height'))  # receive height int value from method post
+    weight = int(request.POST.get('weight'))  # receive weight int value from method post
+    age = int(request.POST.get('age'))        # receive age int value from method post
+    if sex == 'Male' :                                       # case sex value = Male
+        bmr = int(66+((13.7*weight)+(5*height)-(6.8*age)))   # calculate BMR value for male
+    else:                                                    # case sex value = Female
+        bmr = int(665+((9.6*weight)+(1.8*height)-(4.7*age))) # calculate BMR value for female
 
     # excess_calories
-    excess_calories = calories_select_total-bmr
+    excess_calories = calories_select_total-bmr     # excess_calories from difference calories_select_total-bmr
 
-    if(excess_calories < 1):
+    if(excess_calories < 1):                                   # if excess_calories < 1
         contex = { 'sex' : sex,'height' : height,'weight' : weight,'age' : age,'bmr_value' : bmr, 'excess_calories' : excess_calories}
-        return render(request, 'detail_bmr_less.html', contex)
-    else:
+        return render(request, 'detail_bmr_less.html', contex) # render template detail_bmr_less 
+    else:                                                      # if excess_calories > 1 
         contex = { 'sex' : sex,'height' : height,'weight' : weight,'age' : age,'bmr_value' : bmr, 'excess_calories' : excess_calories}
-        return render(request, 'detail_bmr.html', contex)
+        return render(request, 'detail_bmr.html', contex)      # render template detail_bmr 
 
-def burn_calories(request,excess_calories):
-    exercises = Exercise.objects.all()
-    select = exercises.get(pk = request.POST.get('exercise'))
-    contex = { 'exercises' : exercises,'exercise_select' : select, 'excess_calories' : excess_calories}
-    return render(request, 'exercise.html', contex)
+def exercise(request,excess_calories):              # exercise page
+    contex = {'exercises' : Exercise.objects.all(),'excess_calories' : excess_calories}  # argument of send to template
+    return render(request, 'exercise.html',contex)  # render template exercise
 
 
-def sum_calories(foodList):
-    calories_total = 0
-    for food in foodList:
-        calories_total += food.calories_per_menu
-    return calories_total     
+def burn_calories(request,excess_calories):         # function burn_calories for check type exercise at select        
+    exercises = Exercise.objects.all()              # all exercises
+    select = exercises.get(pk = request.POST.get('exercise')) # type exercise at select
+    contex = { 'exercises' : exercises,'exercise_select' : select, 'excess_calories' : excess_calories} # argument of send to template
+    return render(request, 'exercise.html', contex) # render template exercise
+
+
+   
 
 
     
