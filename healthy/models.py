dc36204b0123b0ba67bcd5fdc4e0d6f4c5ed0153
user <a5710131a@gmail.com>
2017-07-01 20:28:56 +0700

    comment all template

--- healthy/models.py
+++ healthy/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# model Food for save menu (name and calories)
 class Food(models.Model):
     name = models.CharField(max_length = 200)
     calories = models.IntegerField()
@@ -8,16 +8,19 @@ class Food(models.Model):
     def __str__(self):
        return self.name
 
+# model FoodList for save selected menu 
 class FoodList(models.Model):
-    name = models.CharField(max_length = 200)
-    number_per_menu = models.IntegerField()
-    calories_per_menu = models.IntegerField()
+    name = models.CharField(max_length = 200) # name menu
+    number_per_menu = models.IntegerField()   # number(Bowl or plate ) per menu
+    calories_per_menu = models.IntegerField() # calories per menu
+
     def __str__(self):
         return self.name
 
+# model Exercise for save types of exercise and ability to burn
 class Exercise(models.Model):
-    types = models.CharField(max_length = 200)
-    burn = models.IntegerField()
+    types = models.CharField(max_length = 200) # types of exercise
+    burn = models.IntegerField()               # ability to burn
     
     def __str__(self):
         return self.types
